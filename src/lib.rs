ccbf745d223fd1d31698e7f056eba8cdd422584a
Super-King-Crimson <129329889+Super-King-Crimson@users.noreply.github.com>
2023-07-09 23:10:13 -0400

    created output.txt to practice writing to stderr

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{fs, error::Error};
+use std::{fs, error::Error, env};
 
 //Wait, all parse_config does is make a Config
 //Let's just impl it!
@@ -6,6 +6,7 @@ use std::{fs, error::Error};
 pub struct Config {
     pub query: String,
     pub path_to_file: String,
+    pub ignore_case: bool,
 }
 
 //ok refactor time!
@@ -20,7 +21,11 @@ pub fn parse_config(args: &[String]) -> /* (&str, &str) */ Config {
     let query = args[1].clone();
     let path_to_file = args[2].clone();
 
-    Config { query, path_to_file }
+    
+    //Alrighy t 
+
+
+    Config { query, path_to_file, ignore_case: false }
 
     // (query, path_to_file)
     //Why are we returning a tuple, then immediately breaking the tuple up?
@@ -33,13 +38,16 @@ impl Config {
     pub fn build(args: &[String]) -> Result<Config, &'static str> {
         //And let's add an error message!
         if args.len() < 3 {
-            return Err("Not enough arguments: Did you forget to include the path to file?");
+            return Err("Not enough arguments: Did you forget to include the path to file and/or string to look for?");
         }
 
         let query = args[1].clone();
         let path_to_file = args[2].clone();
 
-        Ok(Config { query, path_to_file })
+        //We don't care abt the value of IGNORE_CASE, just that it got a value (not unset/false)
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config { query, path_to_file, ignore_case })
     }
 }
 
@@ -47,9 +55,96 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
     let contents = fs::read_to_string(config.path_to_file)?;
     
-    println!("Contents of file: \n------------------------\n{contents}\n------------------------");
+    // println!("Contents of file: \n------------------------\n{contents}\n------------------------");
+
+    let matches: Vec<&str> = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in matches {
+        println!("{line}");
+    }
 
     Ok(())
 }
 
-//Alright now that everything's in lib let's just use it for now
+//By the way, this is good, but not the best: it doesn't take full advantage of iterators
+//Come back after Chapter 13 to try and refactor this!
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut matching_lines = vec![];
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            matching_lines.push(line);
+        }
+    }
+
+    matching_lines
+}   
+
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    //to_lowercase returns a new String
+    let query = query.to_lowercase();
+    
+    let mut matching_lines = vec![];
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            matching_lines.push(line);
+        }
+    }
+
+    matching_lines
+}   
+
+//Alright now that everything's in lib let's just use it from now on
+//Ok tests time!
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn search_test() {
+        let query = "s the";
+
+        //we put a backslash to make rust ignore this newline
+        let contents = "\
+High was the sky,
+deep was the forest,
+unmoving were the people.";
+        
+        //let's make a barebones search function so this test will compile (it will fail)
+        //ok now let's actually implement search so this runs correctly
+        assert_eq!(
+            vec!["High was the sky,", "deep was the forest,"],  
+            search(query, contents)
+        );
+    }
+
+    
+//Ok we did it! CLI tool done!
+//Let's make some environment variables, like case insensitive search
+
+//we can set our env variable (in this case IGNORE_CASE) in Powershell by doing
+// $Env:IGNORE_CASE=[anything]; [whatever command, like cargo run]
+
+    #[test]
+    fn search_case_insensitive_test() {
+        let contents = "\
+God I love RusT
+RUST is so great
+I hate TypeScript
+It's not as cool as Bust";
+
+        let query = "RUST";
+        
+        assert_eq!(vec!["God I love RusT", "RUST is so great"], search_case_insensitive(query, contents));
+    }
+}
+
+//Finally, let's write to stderr (standard error) instead of using println, 
+//for when we want successful program runs to write to a file but errors to display in the console
+//We'll put our errors in output.txt
